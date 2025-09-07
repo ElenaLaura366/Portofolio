6441e794bbe7a5b5627236c03944781e04b2a86f
Gârcineanu Elena-Laura <lauraelena366@gmail.com>
2025-09-07 19:48:45 +0300

    Add random GitHub link opener to Tile component

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -1,6 +1,32 @@
 import { motion } from "motion/react";
 
 function Tile() {
+  const randomLinkGenerator = () => {
+    const links = [
+      "https://github.com/ElenaLaura366/PalChat",
+      "https://github.com/ElenaLaura366/Pizza-delivery-simulator",
+      "https://github.com/ElenaLaura366/Tasks-Tracker",
+      "https://github.com/ElenaLaura366/MedSAM_App",
+      "https://github.com/ElenaLaura366/Noir-Restaurant",
+      "https://github.com/ElenaLaura366/Knucklebones-Dice",
+      "https://github.com/ElenaLaura366/Game-Of-Life",
+      "https://github.com/ElenaLaura366/GithubManager",
+      "https://github.com/ElenaLaura366/Dictionary",
+      "https://github.com/ElenaLaura366/TIC-TAC-TOE",
+      "https://github.com/ElenaLaura366/Multi-Layer-Perceptron",
+      "https://github.com/ElenaLaura366/Logistic-Regression",
+      "https://github.com/ElenaLaura366/Checkers",
+      "https://github.com/ElenaLaura366/Supermarket",
+    ];
+    const randomIndex = Math.floor(Math.random() * links.length);
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    console.log(randomIndex);
+    link.href = links[randomIndex];
+    link.target = "_blank";
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <motion.div
       whileHover={{
@@ -12,6 +38,7 @@ function Tile() {
         ease: "easeOut",
       }}
       className="aspect-square"
+      onClick={randomLinkGenerator}
     />
   );
 }
